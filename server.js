8fc6bae91b40a45ea8447e710ec0074eeccbcf4d
ayeshrodz <ayeshrodz@gmail.com>
2021-03-31 23:54:20 +0530

    v1.0.1 - Express Server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { config } from "dotenv";
+
+//Load env variables
+config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
